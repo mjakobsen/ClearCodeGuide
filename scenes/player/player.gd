31b57bcaf0078268c5e049f1d48da9c4694163ae
Michael Jakobsen <michaelljakobsen@gmail.com>
2024-01-29 21:33:53 +0100

    initial commit

---
+++ scenes/player/player.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+func _process(delta):
+	
+	# input
+	var direction = Input.get_vector("left", "right", "up", "down")
+	position += direction * 200 * delta
+	
+	# laser shooting input
+	if Input.is_action_pressed("primary action"):
+		print('shoot laser')
+		
+	if Input.is_action_pressed("secondary action"):
+		print('shoot granade')
+	
